295494466338cefa72e055c4cc26411b09cad4d2
Orlando <isc.orangelc@gmail.com>
2018-09-25 17:59:16 -0500

    Test protractor

--- delete.mail.spec.js
+++ delete.mail.spec.js
@@ -1,12 +1,12 @@
-//var MailPage = require('./mail.page');
 describe('Protractor Demo App', function() {
+
     it('should add one and two', function() {
-      browser.get('http://juliemr.github.io/protractor-demo/');
-      element(by.model('first')).sendKeys(1);
-      element(by.model('second')).sendKeys(2);  
-      element(by.id('gobutton')).click();  
-      expect(element(by.binding('latest')).getText()).
-          toEqual('3');
-       browser.sleep(3000);
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys(1);
+        element(by.model('second')).sendKeys(2);    
+        element(by.id('gobutton')).click();    
+        expect(element(by.binding('latest')).getText()).
+            toEqual('3');
     });
+
   });
